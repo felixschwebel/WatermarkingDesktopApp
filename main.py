b9ee2a42cd03176daa6a165d57cf5ffa5775bd76
felixschwebel <schwebel.felix@gmail.com>
2022-11-17 12:58:51 +0100

    image upload function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # example.jpg by Jonny Gios from unsplash.com
 
 import tkinter
-from tkinter import ttk
+from tkinter import ttk, filedialog
 import sys
 from PIL import Image, ImageDraw, ImageTk
 
@@ -18,14 +18,17 @@ window = tkinter.Tk()
 window.title('Watermarking App')
 window.config(bg=BG, padx=PAD)
 window.minsize()
-#window.resizable(False, False)
+window.resizable(False, False)
 
 
-# Functions
-def load_picture():
-    canvas.itemconfig(img_container, image=user_image)
+# ---------------------------- Functions ----------------------------
+def upload_image():
+    global USER_IMG
+    USER_IMG = prepare_img(get_file())
+    canvas.itemconfig(img_container, image=USER_IMG)
 
-def save_picture():
+
+def download_image():
     pass
 
 
@@ -33,24 +36,36 @@ def preview_picture():
     pass
 
 
-def get_user_img():
-    raw_img = Image.open('example.jpg')
+def get_file():
+    file_path = filedialog.askopenfilename(
+        initialdir="/",
+        title='Select your Image',
+        filetypes=[('Image files', '*.jpg'), ('Image files', '*.png')]
+    )
+    return file_path
+
+
+def prepare_img(file_path):
+    raw_img = Image.open(file_path)
     resize_factor = 1
     while raw_img.width > CANVAS_WIDTH or raw_img.height > CANVAS_HEIGHT:
         resize_factor -= 0.01
-        raw_img = raw_img.resize((int(raw_img.width*resize_factor), int(raw_img.height*resize_factor)))
+        raw_img = raw_img.resize((int(raw_img.width * resize_factor), int(raw_img.height * resize_factor)))
     final_img = ImageTk.PhotoImage(raw_img)
     return final_img
 
 
+# ---------------------------- UI ----------------------------
 # Example Image
-canvas = tkinter.Canvas(width=CANVAS_WIDTH, height=CANVAS_HEIGHT, background=BG, borderwidth=0, highlightthickness=0)
+canvas = tkinter.Canvas(width=CANVAS_WIDTH, height=CANVAS_HEIGHT,
+                        background=BG, borderwidth=0, highlightthickness=0)
 photo_img = tkinter.PhotoImage(file='start.png')
 
-# User Image
-user_image = get_user_img()
+# User Image GLOBAL Variable
+USER_IMG = None
 
-img_container = canvas.create_image(int(CANVAS_WIDTH/2), int(CANVAS_HEIGHT/2), image=photo_img, anchor=tkinter.CENTER)
+img_container = canvas.create_image(int(CANVAS_WIDTH/2), int(CANVAS_HEIGHT/2),
+                                    image=photo_img, anchor=tkinter.CENTER)
 canvas.grid(column=0, row=1, columnspan=3)
 
 
@@ -65,11 +80,11 @@ text2.grid(column=0, row=3, sticky=tkinter.W, pady=PAD)
 
 
 # Buttons
-upload = tkinter.Button(text='Upload Image', command=load_picture)
+upload = tkinter.Button(text='Upload Image', command=upload_image)
 upload.config(font=FONT, fg=FG, bd=0, relief='flat', pady=5)
 upload.grid(column=0, row=0, sticky=tkinter.W, pady=PAD)
 
-download = tkinter.Button(text='Download Image', command=save_picture)
+download = tkinter.Button(text='Download Image', command=download_image)
 download.config(font=FONT, fg=FG, bd=0, relief='flat', pady=5)
 download.grid(column=2, row=0, sticky=tkinter.E, pady=PAD)
 
@@ -92,7 +107,6 @@ position['values'] = (
     'center',
 )
 position.grid(column=1, row=3, sticky=tkinter.W)
-
 
 
 window.mainloop()
