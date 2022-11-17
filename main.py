8fdbceeb5a2a24521277777e37d70da701a4c1e4
felixschwebel <schwebel.felix@gmail.com>
2022-11-17 12:29:07 +0100

    resizing and displaying an image - function

--- main.py
+++ main.py
@@ -8,15 +8,48 @@ FONT = ('SF Pro', 18, "bold")
 BG = 'white'
 FG = '#565656'
 PAD = 30
+CANVAS_HEIGHT = 500
+CANVAS_WIDTH = 800
 
 # Window
 window = tkinter.Tk()
 window.title('Watermarking App')
 window.config(bg=BG, padx=PAD)
 window.minsize()
-window.resizable(False, False)
+#window.resizable(False, False)
+
 
 # Functions
+def load_picture():
+    canvas.itemconfig(img_container, image=user_image)
+
+def save_picture():
+    pass
+
+
+def preview_picture():
+    pass
+
+
+def get_user_img():
+    raw_img = Image.open('example.jpg')
+    resize_factor = 1
+    while raw_img.width > CANVAS_WIDTH or raw_img.height > CANVAS_HEIGHT:
+        resize_factor -= 0.01
+        raw_img = raw_img.resize((int(raw_img.width*resize_factor), int(raw_img.height*resize_factor)))
+    final_img = ImageTk.PhotoImage(raw_img)
+    return final_img
+
+
+# Example Image
+canvas = tkinter.Canvas(width=CANVAS_WIDTH, height=CANVAS_HEIGHT, background=BG, borderwidth=0, highlightthickness=0)
+photo_img = tkinter.PhotoImage(file='start.png')
+
+# User Image
+user_image = get_user_img()
+
+img_container = canvas.create_image(int(CANVAS_WIDTH/2), int(CANVAS_HEIGHT/2), image=photo_img, anchor=tkinter.CENTER)
+canvas.grid(column=0, row=1, columnspan=3)
 
 
 # Labels
@@ -30,15 +63,15 @@ text2.grid(column=0, row=3, sticky=tkinter.W, pady=PAD)
 
 
 # Buttons
-upload = tkinter.Button(text='Upload Image')
+upload = tkinter.Button(text='Upload Image', command=load_picture)
 upload.config(font=FONT, fg=FG, bd=0, relief='flat', pady=5)
 upload.grid(column=0, row=0, sticky=tkinter.W, pady=PAD)
 
-download = tkinter.Button(text='Download Image')
+download = tkinter.Button(text='Download Image', command=save_picture)
 download.config(font=FONT, fg=FG, bd=0, relief='flat', pady=5)
 download.grid(column=2, row=0, sticky=tkinter.E, pady=PAD)
 
-preview = tkinter.Button(text='Preview')
+preview = tkinter.Button(text='Preview', command=preview_picture)
 preview.config(font=FONT, fg=FG, bd=0, relief='flat', pady=5)
 preview.grid(column=2, row=3, sticky=tkinter.E)
 
@@ -59,14 +92,5 @@ position['values'] = (
 position.grid(column=1, row=3, sticky=tkinter.W)
 
 
-# Example Image
-resize_factor = 2
-width = int(1200 / resize_factor)
-height = int(700 / resize_factor)
-img = Image.open('test.png').resize((width, height))
-photo_img = ImageTk.PhotoImage(img)
-example_img = tkinter.Label(image=photo_img)
-example_img.config(bd=0)
-example_img.grid(column=0, row=1, columnspan=3)
 
 window.mainloop()
