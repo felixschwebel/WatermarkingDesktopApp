fc4e02516f578113debaff17f49905c6f2376155
felixschwebel <schwebel.felix@gmail.com>
2022-11-18 18:23:38 +0100

    choose font, size and color of watermark

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from PIL import Image, ImageDraw, ImageTk
 
 # Apperance Configurations
 FONT = ('SF Pro', 18, "bold")
-WATERMARK_FONT = ('SF Pro', 24, "bold")
 BG = 'white'
 FG = '#565656'
-PAD = 30
+PAD = 20
 CANVAS_HEIGHT = 500
 CANVAS_WIDTH = 800
 WATERMARK_EDGE_DISTANCE = 35
@@ -38,29 +37,28 @@ def download_image():
 def preview_picture():
     global text_container
     canvas.delete(text_container)
-    print(USER_IMG_WIDTH)
-    print(USER_IMG_HEIGHT)
 
+    # Get the position the user selected
     watermark_pos = position.get()
     # 'top left'
     if watermark_pos == 'top left':
-        width = int((CANVAS_WIDTH-USER_IMG_WIDTH)/2)+WATERMARK_EDGE_DISTANCE
-        height = int((CANVAS_HEIGHT-USER_IMG_HEIGHT)/2)+WATERMARK_EDGE_DISTANCE
+        width = int((CANVAS_WIDTH - USER_IMG_WIDTH) / 2) + WATERMARK_EDGE_DISTANCE
+        height = int((CANVAS_HEIGHT - USER_IMG_HEIGHT) / 2) + WATERMARK_EDGE_DISTANCE
         anchor = tkinter.NW
     # 'top right'
     elif watermark_pos == 'top right':
-        width = int((CANVAS_WIDTH-USER_IMG_WIDTH)/2+USER_IMG_WIDTH)-WATERMARK_EDGE_DISTANCE
-        height = int((CANVAS_HEIGHT-USER_IMG_HEIGHT)/2)+WATERMARK_EDGE_DISTANCE
+        width = int((CANVAS_WIDTH - USER_IMG_WIDTH) / 2 + USER_IMG_WIDTH) - WATERMARK_EDGE_DISTANCE
+        height = int((CANVAS_HEIGHT - USER_IMG_HEIGHT) / 2) + WATERMARK_EDGE_DISTANCE
         anchor = tkinter.NE
     # 'bottom left'
     elif watermark_pos == 'bottom left':
-        width = int((CANVAS_WIDTH-USER_IMG_WIDTH)/2)+WATERMARK_EDGE_DISTANCE
-        height = int((CANVAS_HEIGHT-USER_IMG_HEIGHT)/2+USER_IMG_HEIGHT)-WATERMARK_EDGE_DISTANCE
+        width = int((CANVAS_WIDTH - USER_IMG_WIDTH) / 2) + WATERMARK_EDGE_DISTANCE
+        height = int((CANVAS_HEIGHT - USER_IMG_HEIGHT) / 2 + USER_IMG_HEIGHT) - WATERMARK_EDGE_DISTANCE
         anchor = tkinter.SW
     # 'bottom right'
     elif watermark_pos == 'bottom right':
-        width = int((CANVAS_WIDTH-USER_IMG_WIDTH)/2+USER_IMG_WIDTH)-WATERMARK_EDGE_DISTANCE
-        height = int((CANVAS_HEIGHT-USER_IMG_HEIGHT)/2+USER_IMG_HEIGHT)-WATERMARK_EDGE_DISTANCE
+        width = int((CANVAS_WIDTH - USER_IMG_WIDTH) / 2 + USER_IMG_WIDTH) - WATERMARK_EDGE_DISTANCE
+        height = int((CANVAS_HEIGHT - USER_IMG_HEIGHT) / 2 + USER_IMG_HEIGHT) - WATERMARK_EDGE_DISTANCE
         anchor = tkinter.SE
     # 'center'
     else:
@@ -68,8 +66,26 @@ def preview_picture():
         height = int(CANVAS_HEIGHT / 2)
         anchor = tkinter.CENTER
 
+    # Get the size, color and type of the font
+    if text_size.get() != '':
+        size = int(text_size.get())
+    else:
+        size = 24
+
+    if text_color.get() != '':
+        color = text_color.get()
+    else:
+        color = "black"
+
+    if text_font.get() != '':
+        font = text_font.get()
+    else:
+        font = 'SF Pro'
+
     text_container = canvas.create_text(width, height, anchor=anchor,
-                                        text=personal_watermark.get(), font=WATERMARK_FONT)
+                                        text=personal_watermark.get(),
+                                        font=(font, size, 'normal'),
+                                        fill=color)
 
 
 def get_file():
@@ -108,23 +124,32 @@ canvas = tkinter.Canvas(width=CANVAS_WIDTH, height=CANVAS_HEIGHT,
                         background=BG, borderwidth=0, highlightthickness=0)
 photo_img = tkinter.PhotoImage(file='start.png')
 
-
 # User Image
-img_container = canvas.create_image(int(CANVAS_WIDTH/2), int(CANVAS_HEIGHT/2),
+img_container = canvas.create_image(int(CANVAS_WIDTH / 2), int(CANVAS_HEIGHT / 2),
                                     image=photo_img, anchor=tkinter.CENTER)
-text_container = canvas.create_text(int(CANVAS_WIDTH/2), int(CANVAS_HEIGHT/2), text='')
-canvas.grid(column=0, row=1, columnspan=3)
-
+text_container = canvas.create_text(int(CANVAS_WIDTH / 2), int(CANVAS_HEIGHT / 2), text='')
+canvas.grid(column=0, row=1, columnspan=4)
 
 # Labels
-text1 = tkinter.Label(text='Your Watermark Text')
-text1.config(bg=BG, fg=FG, font=FONT)
-text1.grid(column=0, row=2, sticky=tkinter.W, pady=PAD)
+text_mark = tkinter.Label(text='Your Watermark Text')
+text_mark.config(bg=BG, fg=FG, font=FONT)
+text_mark.grid(column=0, row=2, sticky=tkinter.W, pady=PAD)
 
-text2 = tkinter.Label(text='Position')
-text2.config(bg=BG, fg=FG, font=FONT)
-text2.grid(column=0, row=3, sticky=tkinter.W, pady=PAD)
+text_position = tkinter.Label(text='Position')
+text_position.config(bg=BG, fg=FG, font=FONT)
+text_position.grid(column=0, row=3, sticky=tkinter.W, pady=PAD)
 
+text_font = tkinter.Label(text='Text Font')
+text_font.config(bg=BG, fg=FG, font=FONT)
+text_font.grid(column=2, row=3, sticky=tkinter.W, pady=PAD)
+
+text_color = tkinter.Label(text='Text Color')
+text_color.config(bg=BG, fg=FG, font=FONT)
+text_color.grid(column=0, row=4, sticky=tkinter.W, pady=PAD)
+
+text_size = tkinter.Label(text='Text Size')
+text_size.config(bg=BG, fg=FG, font=FONT)
+text_size.grid(column=2, row=4, sticky=tkinter.W, pady=PAD)
 
 # Entry
 personal_watermark = tkinter.Entry(width=42)
@@ -140,6 +165,34 @@ position['values'] = (
 )
 position.grid(column=1, row=3, sticky=tkinter.W)
 
+text_font = tkinter.ttk.Combobox(width=15, state='readonly')
+text_font['values'] = (
+    'Arial',
+    'Courier',
+    'SF Pro',
+    'Times New Roman',
+)
+text_font.grid(column=3, row=3, sticky=tkinter.W)
+
+text_color = tkinter.ttk.Combobox(width=15, state='readonly')
+text_color['values'] = (
+    'black',
+    'white',
+    'gray',
+    'gray20',
+    'gray30',
+    'gray40',
+    'gray50',
+    'gray60',
+    'gray70',
+    'gray80',
+    'gray90',
+)
+text_color.grid(column=1, row=4, sticky=tkinter.W)
+
+text_size = tkinter.ttk.Combobox(width=15, state='readonly')
+text_size['values'] = (14, 16, 18, 20, 22, 24, 28, 30, 36, 40, 50, 60)
+text_size.grid(column=3, row=4, sticky=tkinter.W)
 
 # Buttons
 upload = tkinter.Button(text='Upload Image', command=upload_image)
@@ -148,11 +201,10 @@ upload.grid(column=0, row=0, sticky=tkinter.W, pady=PAD)
 
 download = tkinter.Button(text='Download Image', command=download_image)
 download.config(font=FONT, fg=FG, bd=0, relief='flat', pady=5)
-download.grid(column=2, row=0, sticky=tkinter.E, pady=PAD)
+download.grid(column=3, row=0, sticky=tkinter.E, pady=PAD)
 
 preview = tkinter.Button(text='Preview', command=preview_picture)
 preview.config(font=FONT, fg=FG, bd=0, relief='flat', pady=5)
-preview.grid(column=2, row=3, sticky=tkinter.E)
-
+preview.grid(column=3, row=2, sticky=tkinter.E)
 
 window.mainloop()
